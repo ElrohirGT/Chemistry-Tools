f691f8a550d773b680227dc8c45b8572ff13b807
ElrohirGT <elrohirgt@gmail.com>
2022-02-15 20:25:11 -0600

    Add project files.

---
+++ Chemistry Tools/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chemistry_Tools.ViewModels;
+public class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting => "Welcome to Avalonia!";
+}
---
+++ Chemistry Tools/ViewModels/ViewModelBase.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using ReactiveUI;
+
+namespace Chemistry_Tools.ViewModels;
+public class ViewModelBase : ReactiveObject
+{
+}
