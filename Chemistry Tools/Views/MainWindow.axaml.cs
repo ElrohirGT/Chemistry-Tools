c006ebfd77b2ccfae64c7b24b58af770ff5ddb20
ElrohirGT <elrohirgt@gmail.com>
2022-02-16 08:17:33 -0600

    Added the auto updater

--- Chemistry Tools/Views/MainWindow.axaml.cs
+++ Chemistry Tools/Views/MainWindow.axaml.cs
@@ -1,16 +1,30 @@
-using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+
+using NetSparkleUpdater;
+using NetSparkleUpdater.SignatureVerifiers;
 
 namespace Chemistry_Tools.Views;
 public partial class MainWindow : Window
 {
+    private readonly SparkleUpdater _sparkle;
+
     public MainWindow()
     {
         InitializeComponent();
 #if DEBUG
             this.AttachDevTools();
 #endif
+        string key = "+tmxn2gyjXh4VK5AykT5HT/E9qEM34/+GTaspXU1dWA=";
+        _sparkle = new SparkleUpdater(
+            "https://mega.nz/file/9EFWnLaZ#UBeO6LU0_8oolhBKOvNZTRqrVRGth7E4Hbrbb0DbmAY",
+            new Ed25519Checker(NetSparkleUpdater.Enums.SecurityMode.Strict, key))
+        {
+            UIFactory = new NetSparkleUpdater.UI.Avalonia.UIFactory(Icon),
+            ShowsUIOnMainThread = true,
+        };
+
+        _sparkle.StartLoop(true, true);
     }
 
     private void InitializeComponent()
