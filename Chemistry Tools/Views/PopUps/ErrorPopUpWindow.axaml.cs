d0119f283fe2ded0ee89cd6e058b43d6e7e44d79
ElrohirGT <elrohirgt@gmail.com>
2022-02-23 15:25:36 -0600

    Added an error popup

---
+++ Chemistry Tools/Views/PopUps/ErrorPopUpWindow.axaml.cs
@@ -0,0 +1,32 @@
+using System.Reactive;
+
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Avalonia.ReactiveUI;
+
+using Chemistry_Tools.ViewModels;
+
+namespace Chemistry_Tools.Views.PopUps;
+public partial class ErrorPopUpWindow : ReactiveWindow<ErrorPopUpViewModel>
+{
+    public ErrorPopUpWindow()
+    {
+        InitializeComponent();
+#if DEBUG
+        this.AttachDevTools();
+#endif
+        DataContext = ViewModel = new ErrorPopUpViewModel();
+        ViewModel.Close -= CloseWindow;
+        ViewModel.Close += CloseWindow;
+    }
+
+    private void CloseWindow(Unit obj) => Close(obj);
+
+    public IControl? ErrorContainer { get; internal set; }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
