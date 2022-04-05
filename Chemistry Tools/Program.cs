268fb89af0bb211d3200713ab44f5becbd90c76d
ElrohirGT <elrohirgt@gmail.com>
2022-04-05 14:44:27 -0600

    Logs any unhandled errors to a file.

--- Chemistry Tools/Program.cs
+++ Chemistry Tools/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 
 using Avalonia;
 using Avalonia.ReactiveUI;
@@ -15,8 +16,49 @@ internal class Program
     [STAThread]
     public static void Main(string[] args)
     {
-        AppBootstrapper.Register(Locator.CurrentMutable, Locator.Current);
-        BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+        try
+        {
+            var appDomain = AppDomain.CurrentDomain.BaseDirectory;
+            Directory.SetCurrentDirectory(appDomain);
+            AppBootstrapper.Register(Locator.CurrentMutable, Locator.Current);
+            BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+        }
+        catch (Exception e)
+        {
+            string fileName = "chToolsLogFile.txt";
+            var currentWorkingDir = Directory.GetCurrentDirectory();
+            var exePath = System.Reflection.Assembly.GetEntryAssembly()?.Location;
+            var appDomain = AppDomain.CurrentDomain.BaseDirectory;
+
+            using var stream = File.CreateText(Path.Combine(appDomain, fileName));
+            stream.WriteLine(new string('=', 15));
+            stream.WriteLine($"Current Working Directory: {currentWorkingDir}");
+            stream.WriteLine($"EXE Path: {exePath}");
+            stream.WriteLine($"App Domain: {appDomain}");
+            stream.WriteLine(new string('=', 15));
+            stream.WriteLine();
+
+            LogExceptionToFile(e, stream);
+
+            throw;
+        }
+    }
+
+    private static void LogExceptionToFile(Exception? e, StreamWriter stream, int padding = 0)
+    {
+        if (e is null)
+            return;
+        if (e is AggregateException aggregate)
+            foreach (var exception in aggregate.InnerExceptions)
+                LogExceptionToFile(exception, stream);
+
+        while (e is not null)
+        {
+            stream.WriteLine($"{new string('\t', padding)}{e}");
+            LogExceptionToFile(e.InnerException, stream, padding+1);
+            e = e.InnerException;
+        }
+        stream.Flush();
     }
 
     // Avalonia configuration, don't remove; also used by visual designer.
