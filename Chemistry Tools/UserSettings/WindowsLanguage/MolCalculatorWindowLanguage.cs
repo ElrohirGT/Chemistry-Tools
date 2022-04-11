448f2bc9a4983981604c09a5ccc4b428af2517cb
ElrohirGT <elrohirgt@gmail.com>
2022-04-11 15:46:33 -0600

    Removed the useless property of the language files

--- Chemistry Tools/UserSettings/WindowsLanguage/MolCalculatorWindowLanguage.cs
+++ Chemistry Tools/UserSettings/WindowsLanguage/MolCalculatorWindowLanguage.cs
@@ -5,7 +5,6 @@ public class MolCalculatorWindowLanguage
 {
     public string Title { get; set; }
     public string TextBoxWatermark { get; set; }
-    public string MainActionButton { get; set; }
     public string ParseMoleculeErrorMessage { get; set; }
     public string SuccessMessageFormat { get; set; }
     public string InstructionsTitle { get; set; }
